1853a7bc59cb0a0ac4061425aceb6ba1ab880afb
Behrouz Shamsaei <behrooz_sh62@yahoo.com>
2018-06-13 16:04:06 -0400

    The background body wallpaper added

---
+++ src/main/java/edu/uc/eh/PiptmApplication.java
@@ -0,0 +1,12 @@
+package edu.uc.eh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PiptmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PiptmApplication.class, args);
+	}
+}
